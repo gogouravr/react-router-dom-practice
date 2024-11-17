49f78ca04fbcd0a67ef56435141196f57608d829
gouravrajbit <gouravr244@gmail.com>
2024-11-17 19:27:52 +0530

    Add boilerplate code

--- frontend/src/pages/Event.jsx
+++ frontend/src/pages/Event.jsx
@@ -1,26 +1,37 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react';
 
-const EVENTS = [
-    {
-        id: 'e1',
-        title: 'First event'
-    },
-    {
-        id: 'e2',
-        title: 'Second event'
-    }
-]
+import EventsList from '../components/EventsList';
 
-export default function EventsPage() {
+function EventsPage() {
+    const [isLoading, setIsLoading] = useState(false);
+    const [fetchedEvents, setFetchedEvents] = useState();
+    const [error, setError] = useState();
+
+    useEffect(() => {
+        async function fetchEvents() {
+            setIsLoading(true);
+            const response = await fetch('http://localhost:8080/events');
+
+            if (!response.ok) {
+                setError('Fetching events failed.');
+            } else {
+                const resData = await response.json();
+                setFetchedEvents(resData.events);
+            }
+            setIsLoading(false);
+        }
+
+        fetchEvents();
+    }, []);
     return (
-        <div>
-            <h1>Events Page</h1>
-            <ul>
-                {EVENTS.map(event => <li key={event.id}>
-                    <Link to={event.id}>{event.title} </Link>
-                </li>)}
-            </ul>
-        </div>
-    )
+        <>
+            <div style={{ textAlign: 'center' }}>
+                {isLoading && <p>Loading...</p>}
+                {error && <p>{error}</p>}
+            </div>
+            {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
+        </>
+    );
 }
+
+export default EventsPage;
